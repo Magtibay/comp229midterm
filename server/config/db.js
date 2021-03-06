edf8088a19fc3999dcb73daec82cb134ecbb211d
Michael Magtibay <mamgtiba@my.centennialcollege.ca>
2021-03-06 10:24:43 -0500

    completed midterm

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+/**
+ * Add your code here to fix URI connection
+ * 
+ */
+ module.exports =
+ {
+    "URI": "mongodb+srv://User:yRQWPsV6HKIQbinK@dbserver.adzdf.mongodb.net/book_store?retryWrites=true&w=majority"
+
+ }
